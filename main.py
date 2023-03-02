ef6a772cccc086cae7f81c4ee036ab7705708330
OGRESKULL <admin@ogreskull.com>
2023-03-02 10:13:53 -0800

    clean up

---
+++ main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+import TheNumberForge
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("sigh")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
