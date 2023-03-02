8f3c3e2a2a996dec87e36559306a2c084c2c7a9f
OGRESKULL <admin@ogreskull.com>
2023-03-02 10:02:03 -0800

    clean up

--- TheNumberForge/KW1.py
+++ TheNumberForge/KW1.py
@@ -1,6 +1,6 @@
-def kw1solver(milled_key_length, milled_key_depth, milled_key_mac):
+def mac_cruncher(milled_key_length, milled_key_depth, milled_key_mac):
     if (milled_key_depth + milled_key_mac + milled_key_length) < 4:
-        return "don't mess with me"
+        return "don't mess with me, this doesn't look like a valid key schema"
     if milled_key_length == 1:
         return milled_key_depth
     max_key_space = milled_key_depth ** milled_key_length
@@ -14,48 +14,40 @@ def kw1solver(milled_key_length, milled_key_depth, milled_key_mac):
         return max_key_space
 
     else:
-        valid_positions = 0
-
         potential_transforms = 0
         depth_range = range(0, milled_key_depth)
-        non_cringe_set = set()
+        fresh_set = set()
         for j in depth_range:
             for x in depth_range:
                 if abs(j - x) <= milled_key_mac:
-                    non_cringe_set.add((j, x))
+                    fresh_set.add((j, x))
                     potential_transforms += 1
 
         for i in range(1, milled_key_length):
-            print("length position: " + str(i))
-            if i == 1:
-                print(', '.join(str(s) for s in non_cringe_set))
+            #print("length position: " + str(i))
             if i > 1:
-                current_set = non_cringe_set
-                non_cringe_set = set()
+                current_set = fresh_set
+                fresh_set = set()
                 for q in current_set:
-                    print("q: " + str(q))
                     for x in depth_range:
                         if abs(q[-1] - x) <= milled_key_mac:
                             working_lad = list(q)
                             working_lad.append(x)
                             working_lad = tuple(working_lad)
-                            non_cringe_set.add(working_lad)
-                            #print(', '.join(str(s) for s in non_cringe_set))
-                           # non_cringe_set.add(tuple(list(q).append(x)))
-                    # make a new set lol , count the new set at the end :)
-        # print("valid positions " + str(potential_transforms))
-        print(', '.join(str(s) for s in non_cringe_set))
-        [print(i) for i in (sorted(non_cringe_set))]
-        return len(non_cringe_set)
+                            fresh_set.add(working_lad)
+        #print("valid positions " + str(potential_transforms))
+        print(', '.join(str(s) for s in sorted(fresh_set)))
+        return len(fresh_set)
 
 
-print(kw1solver(3,4,1))
-
-assert kw1solver(2,6,6) == 36
-assert kw1solver(2,6,5) == 36
-assert kw1solver(2,6,4) == 34
-assert kw1solver(2,6,3) == 30
-assert kw1solver(2,4,2) == 14
-assert kw1solver(3,3,1) == 17
-assert kw1solver(3,4,2) == 50
-assert kw1solver(3,4,1) == 26
+# print(mac_cruncher(3,4,1))
+print(mac_cruncher(5, 6, 4))
+# 
+# assert mac_cruncher(2,6,6) == 36
+# assert mac_cruncher(2,6,5) == 36
+# assert mac_cruncher(2,6,4) == 34
+# assert mac_cruncher(2,6,3) == 30
+# assert mac_cruncher(2,4,2) == 14
+# assert mac_cruncher(3,3,1) == 17
+# assert mac_cruncher(3,4,2) == 50
+# assert mac_cruncher(3,4,1) == 26
